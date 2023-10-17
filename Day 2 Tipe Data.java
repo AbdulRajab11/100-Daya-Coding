a64483e6112f2b34c4bc59614828800f398649ff
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-10-17 20:40:09 +0800

    Create Day 2 Tipe Data.java

---
+++ Day 2 Tipe Data.java
@@ -0,0 +1,46 @@
+package day.pkg2;
+public class Day2 {
+    public static void main(String[] args) {
+
+byte byte1 ;/// Menyimpan angka dari -128 sampai 127
+            byte1 = 127;
+            
+    int int1 ;/// Menyimpan angka dari -2,147,483,648 sampai 2,147,483,647
+            int1 = 200000;
+            
+    short short1 ;/// Menyimpan angka dari -32,768 sampai 32,767
+            short1 = 32717;
+            
+    long long1 ;/// Menyimpan banyak angka :)
+            long1 = 6230000;
+            
+    float float1 ;/// Menyimpan angka desimal
+            float1 = 18.4f ;/// Tipe data float diakhiri dengan simbol "f"
+            
+    double double1 ;/// Menyimpan angka desimal
+            double1 = 21; ///21.0
+            
+    char char1 ;/// Menyimpan satu karakter
+            char1 = 'R' ;
+            
+    boolean boolean1 ;/// Menyimpan nilai true dan false
+                boolean1 = true ;
+                
+    /// TIPE DATA NON PRIMITIF
+    
+    String string1 ;
+            string1 = "Abdul Rajab" ;
+            
+            System.out.println("Ini hasil tipe data byte " + byte1);
+            System.out.println("Ini hasil tipe data integer " + int1);
+            System.out.println("Ini hasil tipe data short " + short1);
+            System.out.println("Ini hasil tipe data long " + long1);
+            System.out.println("Ini hasil tipe data float " + float1);
+            System.out.println("Ini hasil tipe data double " + double1);
+            System.out.println("Ini hasil tipe data char " + char1);
+            System.out.println("Ini hasil tipe data boolean " + boolean1);
+            System.out.println("Ini hasil tipe data String " + string1);
+                        
+    }
+    
+}
