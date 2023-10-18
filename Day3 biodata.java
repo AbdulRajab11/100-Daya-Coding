82e13ca37dd68da94ed6b84c111b2cb3927eaacf
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-10-18 20:51:51 +0800

    Create Day3 biodata.java

---
+++ Day3 biodata.java
@@ -0,0 +1,24 @@
+package day3;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Abdul Rajab";
+        String nim = "D0223518";
+        String kelas = "Informatika E";
+        String fakultas = "Teknik";
+        String prodi = "Informatika";
+        String angkatan = "23";
+        String universitas = "Sulawesi Barat";
+        
+        
+        System.out.println("Nama        : "+nama);
+        System.out.println("NIM         : "+nim);
+        System.out.println("Kelas       : "+kelas);
+        System.out.println("Fakultas    : "+fakultas);
+        System.out.println("Prodi       : "+prodi);
+        System.out.println("Angkatan    : "+angkatan);
+        System.out.println("Universitas : "+universitas);
+    }
+    
+}
