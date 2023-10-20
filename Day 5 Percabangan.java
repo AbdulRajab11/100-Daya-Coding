5838cfe56c852a5d2226447c995d6d87ce76021f
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-10-21 01:15:27 +0800

    Create Day 5 Percabangan.java

---
+++ Day 5 Percabangan.java
@@ -0,0 +1,13 @@
+package day5;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        if(7<6){
+           System.out.println("benar");
+        }else{
+            System.out.println("salah");
+        }
+    }
+    
+}
