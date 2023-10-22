164a0230e801b19400c750d060ebd7f9f4aee444
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-10-23 00:43:55 +0800

    Create day 7 perulangan.java

---
+++ day 7 perulangan.java
@@ -0,0 +1,12 @@
+package day7;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        for (byte i=-1; i<13; i++){
+            System.out.println(i+1);
+        }
+        
+    }
+    
+}
