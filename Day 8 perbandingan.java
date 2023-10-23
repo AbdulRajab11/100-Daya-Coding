21c246b9995314d143d04171d741ebf3b462666c
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-10-24 00:02:52 +0800

    Create Day 8 perbandingan.java

---
+++ Day 8 perbandingan.java
@@ -0,0 +1,19 @@
+package day 8;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+    int a = 10;
+    int b = 5;
+    boolean hasil;
+     
+    hasil = a == b;
+    System.out.println("Apakah a == b ? " + hasil );   
+     
+    hasil = a != b;
+    System.out.println("Apakah a != b ? " + hasil );   
+     
+    }
+}
