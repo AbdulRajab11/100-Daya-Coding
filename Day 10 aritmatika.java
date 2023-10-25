e9c9fbac9bcb7bd14b437054d77dd4ea24c82962
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-10-25 21:34:40 +0800

    Create Day 10 aritmatika.java

---
+++ Day 10 aritmatika.java
@@ -0,0 +1,13 @@
+package day10;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        int angka1 = 13;
+         int angka2 = 35;
+          System.out.println("angka1 x angka2 = " + (angka1%angka2));
+    }
+    
+}
