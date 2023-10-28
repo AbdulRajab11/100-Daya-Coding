e176e2438d2d551f1fe09eec8efc07139a5036c9
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-10-28 21:05:45 +0800

    Create Day 13 percabangan.java

---
+++ Day 13 percabangan.java
@@ -0,0 +1,12 @@
+package day13;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 6
+        if(a>0 && a%2==0){
+            System.out.println(a);
+        }
+    }
+    
+}
