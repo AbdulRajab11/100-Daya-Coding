f721c1c46a75befbd6c2c49b3ce4a14930c143dd
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-10-29 09:45:29 +0800

    Create Day 14 percabangan.java

---
+++ Day 14 percabangan.java
@@ -0,0 +1,16 @@
+package day14;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 6
+        if(a>=1 && a<=9){
+            System.out.println("satuan");
+        }if(a>=10 && a<=99){
+            System.out.println("puluhan");
+        }if(a>=100 && a<=999){
+            System.out.println("ratusan");
+        }
+    }
+    
+}
