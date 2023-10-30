8eac35d76ad48d90571f0e5292da935674cf8266
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-10-30 21:53:24 +0800

    Create Day 15 while loop.java

---
+++ Day 15 while loop.java
@@ -0,0 +1,14 @@
+package day 15;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int a = 5;
+        while (a<9){
+            System.out.println(a);
+            a++;
+        }
+    }
+    
+}
