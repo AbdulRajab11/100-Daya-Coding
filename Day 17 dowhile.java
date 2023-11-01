942f636b281569cffc55837773f97f3deb177109
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-11-01 22:54:53 +0800

    Create Day 17 dowhile.java

---
+++ Day 17 dowhile.java
@@ -0,0 +1,14 @@
+package day17;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 1;
+     do {
+     System.out.println(a);
+     a+=2;
+    }
+     while (a < 35);
+    }
+    
+}
