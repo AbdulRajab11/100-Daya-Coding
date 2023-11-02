81972cc3af1bb0960440f1baf55fd0e2a3af1c09
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-11-02 19:49:25 +0800

    Create Day 18 switch.java

---
+++ Day 18 switch.java
@@ -0,0 +1,33 @@
+package day18;
+
+public class Main {
+  public static void main(String[] args) {
+    int Hero = 5;
+    switch (Hero) {
+        case 1:
+        System.out.println("gusion");
+        break;
+        case 2:
+        System.out.println("fanny");
+        break;
+        case 3:
+        System.out.println("Lancelot");
+        break;
+        case 4:
+        System.out.println("Saber");
+        break;
+        case 5:
+        System.out.println("Zilong");
+        break;
+        case 6:
+        System.out.println("Alpha");
+        break;
+        case 7:
+        System.out.println("Popol & Kupa");
+        break;
+        case 8:
+        System.out.println("Lesley");
+        break;
+    }
+  }
+}
