96573a9d2f122d6fc4d83f6c36c4fbe3a9e33b5e
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-11-04 21:29:22 +0800

    Create Day 20 String Length.java

---
+++ Day 20 String Length.java
@@ -0,0 +1,9 @@
+package day20;
+
+public class Main {
+  public static void main(String[] args) {
+    //berfungsi untuk menghitung panjang kata//
+    String txt = "sok-sokan_pilih_jurusan_teknik_informatika_tapi_ngak_maungoding";
+    System.out.println("Panjang Kalimat di atas adalah : " + txt.length());
+  }
+}
