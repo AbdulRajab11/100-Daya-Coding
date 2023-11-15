72f06cc188dcc347f0df7c036fa2ab8f906d90f5
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-11-15 22:20:58 +0800

    Create Day 31 logika&perbandingan.java

---
+++ Day 31 logika&perbandingan.java
@@ -0,0 +1,17 @@
+package day31;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 23;
+        if ( a<=b && a==b){
+            System.out.println("BENAR");
+        }else{
+            System.out.println("SALAH");
+        }
+        
+    }
+    
+}
+    
