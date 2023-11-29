fbc86a16b4c9efefbfed39cd65df32b72af2e95f
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-11-29 09:41:13 +0800

    Memasukkan nama kedalam array

---
+++ Day 45.java
@@ -0,0 +1,14 @@
+package day45;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String[] namateman = {"Uci","Sinta","Sari","Indar","Cici"};
+        
+        for(String nama : namateman){
+        System.out.println(nama);
+            
+        }
+    }
+    
+}
