c807735b27a83b3c33e18cc6863eac5a7727bf60
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-12-01 19:58:11 +0800

    Input array

---
+++ Day 47.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class day47 {
+    public static void main(String[] args){
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan Panjang Index : ");
+        int index = input.nextInt();
+        int a [] = new int[index];
+        System.out.println();
+        System.out.println("Input :");
+        for(int i = 0; i < index; i++){
+            System.out.print("Masukkan Data ke-"+(i+1)+" : ");
+            a[i] = input.nextInt();
+        }
+        System.out.println();
+        System.out.println("Output :");
+        for(int i = 0; i < index; i++){
+            System.out.print(a[i]+" ");
+    }
+    }
+
+        
+        
+    }
+    
