38ee1be3c708442ab8ab83eb901b1383fa659932
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-12-07 23:45:07 +0800

    Mencari Indeks bernilai sama

---
+++ Day 53.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class day53 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int[] angka = {1, 4, 6, 4, 4, 4, 5, 7, 9, 2, 4};
+        
+        System.out.print("Cari indeks : ");
+        int cari = sc.nextInt();
+        int indeks = 0;
+        for (int i = 0; i < angka.length; i++) {
+            if (cari == angka[i]) {
+                indeks = i;
+            }
+        }System.out.println(indeks);
+        
+    }
+    
+}
