dd54cf12735fdd2504df311d946ef8c2fe8dece5
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-12-09 00:03:34 +0800

    Ganjil genap array 1D

---
+++ Day 54.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class day54 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.print("Masukkan jumlah inputan : ");
+        int jumlah = sc.nextInt();
+        
+        int[] nilai = new int [jumlah];
+        
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.print("Masukkan nilai : ");
+            nilai[i] = sc.nextInt();
+        }
+        
+        
+        for (int i = 0; i < nilai.length; i++) {
+            if (nilai[i] %2 == 0) {
+                System.out.println("Genap");
+            }else {
+                System.out.println("Ganjil");
+            }
+        }
+        
+    }
+    
+}
