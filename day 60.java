f5d1decb4a86351f3234c89777ef739d122e430c
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-12-14 20:14:07 +0800

    Menjumlahkan array 2D

---
+++ day 60.java
@@ -0,0 +1,19 @@
+public class day60 {
+    
+    public static void main(String[] args) {
+        int[][] nilai = {
+            {24, 31, 68},
+            {55, 83, 99},
+            {45, 77, 23}
+        };
+        
+        int jumlah = 0;
+        
+        for (int i = 0; i < nilai.length; i++) {
+            for (int j = 0; j < nilai[i].length; j++) {
+                jumlah += nilai[i][j];
+            }
+        }System.out.println(jumlah);
+    }
+    
+}
