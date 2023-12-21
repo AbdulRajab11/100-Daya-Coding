8ebacbe731e86f49a275b428ac9f4e5e80bf7808
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-12-21 19:44:29 +0800

    soal coding war

---
+++ Day 66.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class day66 {
+    public static void main(String[] args) {
+
+        Scanner batas = new Scanner (System.in);
+        System.out.print("Masukkan batas nilai = ");// Input Batas Nilai
+        int batasNilai = batas.nextInt();
+        
+        for (int i = 1; i < batasNilai; i++) {
+            if (i %3 ==0) { // Menentukan modulus sisa bagi
+                System.out.println(i);
+            }
+        }
+        
+    }
+    
+}
