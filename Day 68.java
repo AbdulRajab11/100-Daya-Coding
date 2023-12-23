167b1b88a7892bd8989607cf9f2dbc4fcf09e56f
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2023-12-23 22:15:33 +0800

    Menghitung luas jajargenjang

---
+++ Day 68.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class day68 {
+    static void luas(double a, double t){
+        double L = a * t;
+        System.out.println("\nJika alas "+a+" dan tinggi "+t+" Maka luas jajar genjang adalah "+L);
+    }
+    static void keliling(double a, double t){
+        double k = 2 * (a + t);
+        System.out.println("\nDan kelilingnya adalah "+k);
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan Alas : ");
+        double a = in.nextDouble();
+        System.out.print("Masukkan Tinggi : ");
+        double t = in.nextDouble();
+        
+        luas(a, t);
+        keliling(a, t);
+    }
+    
+}
