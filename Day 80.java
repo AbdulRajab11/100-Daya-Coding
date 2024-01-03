a2fef6c3839c6de4f9e27c2df183c9e6796b694f
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2024-01-03 22:46:44 +0800

    Array Multidimensi

---
+++ Day 80.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class day80 {
+    
+    public static void main(String[] args) {
+        
+        
+        String[][] alamat = {
+            {"Rajab", "(Antalili)"},
+            {"Sinta", "(Salupangkang)"},
+        
+        };
+        /*
+        System.out.print(alamat[0][0]);
+        System.out.println(alamat[0][1]);
+        System.out.print(alamat[1][0]);
+        System.out.println(alamat[1][1]);
+        */
+        
+        System.out.println(Arrays.deepToString(alamat));
+        
+    }
+    
+}
