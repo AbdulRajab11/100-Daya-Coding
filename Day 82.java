53edd8091461d416ad90b9a77d45a21820b5d378
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2024-01-05 23:54:56 +0800

    Array For Each

---
+++ Day 82.java
@@ -0,0 +1,14 @@
+
+public class day82 {
+    
+    public static void main(String[] args) {
+
+        String[] nama = {"Indar", "Uchy", "Nasra", "Cici", "Sinta"};
+        
+        for (String i : nama) {
+            System.out.println(i);
+        }
+
+    }
+    
+}
