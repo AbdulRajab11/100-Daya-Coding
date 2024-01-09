b4c479a4f63c8075ae061b15b19d5565b8728bd8
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2024-01-09 19:58:02 +0800

    Parameter Fungsi

---
+++ Day 86.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class day86 {
+    public static String siswa(String nama, int umur){
+        return nama+" berumur "+umur+"th";
+    }
+    
+    public static void main(String[] args) {
+        String nm;
+        int umr;
+        Scanner s = new Scanner(System.in);
+        System.out.print("nama : ");
+        nm = s.nextLine();
+        System.out.print("umur : ");
+        umr = s.nextInt();
+        System.out.println("\noutput : ");
+        System.out.println(siswa(nm, umr));
+    }
+    
+}
