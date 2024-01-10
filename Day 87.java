bddca89ce861fb22b5da2f1da25e8e19dc078e7e
AbdulRajab11 <148034362+AbdulRajab11@users.noreply.github.com>
2024-01-10 23:33:08 +0800

    Aritmatika dalam prosedur

---
+++ Day 87.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class day87 {
+    public static void operasi(int a, int b){
+        int c;
+        c = a + b;
+        System.out.println(a + " + " + b + " = " + c);
+        c = a - b;
+        System.out.println(a + " - " + b + " = " + c);
+        c = a * b;
+        System.out.println(a + " x " + b + " = " + c);
+        c = a / b;
+        System.out.println(a + " : " + b + " = " + c);
+    }
+    
+    public static void main(String[] args) {
+        int a, b;
+        Scanner s = new Scanner (System.in);
+        System.out.print("Input Nilai a = ");
+        a = s.nextInt();
+        System.out.print("Input Nilai b = ");
+        b = s.nextInt();
+        operasi(a, b);
+    }
+    
+}
